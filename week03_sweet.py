256b497e44c90249b8bc6e7c1a7d5e3fa93558ec
sihwahan <67182962+sihwahan@users.noreply.github.com>
2020-08-10 15:17:47 +0900

    week04hw
    4주차 숙제

--- week03_sweet.py
+++ week03_sweet.py
@@ -11,6 +11,7 @@ db = client.dbsparta
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
+
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
